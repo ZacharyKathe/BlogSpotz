761e298a53b698b59ee994a1ea0e7fb96c60c136
ZacharyKathe <55865084+ZacharyKathe@users.noreply.github.com>
2021-05-01 20:51:19 -0700

    updating models, handlebars, server

---
+++ public/js/script.js
@@ -0,0 +1,75 @@
+document.querySelector("#loginForm").addEventListener("submit",event=>{
+    event.preventDefault();
+    const fetchObj = {
+        email:document.querySelector("#loginEmail").value,
+        password:document.querySelector("#loginPassword").value,
+    }
+    console.log(fetchObj);
+    fetch("api/users/login",{
+        method:"POST",
+        body:JSON.stringify(fetchObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        console.log(res);
+        if(res.ok){
+            console.log("logged in successfully!")
+            location.replace("/profile")
+        } else {
+            alert("log in failed!")
+            location.reload();
+        }
+    })
+})
+
+
+// async function formHandler(event){
+//     event.preventDefault();
+//     event.stopPropagation();
+//     const description = document.querySelector('.userpost').value;
+    
+//     console.log(description)
+//     const response = await fetch("api/projects/posts",{
+//         method: "POST",
+//         body:JSON.stringify(description),
+//         headers:{
+//             "Content-Type":"application/json"
+//         }
+//     });
+
+//     if (response.ok){
+//         document.location.replace('profile')
+//     } else {
+//         alert("failed to add")
+//     }
+// }
+
+// document.querySelector('.userpost').addEventListener('submit', formHandler);
+
+
+document.querySelector("#signupForm").addEventListener("submit",event=>{
+    event.preventDefault();
+    const fetchObj = {
+        email:document.querySelector("#signupEmail").value,
+        password:document.querySelector("#signupPassword").value,
+        name:document.querySelector("#signupUsername").value,
+    }
+    console.log(fetchObj);
+    fetch("/api/users/signup",{
+        method:"POST",
+        body:JSON.stringify(fetchObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        console.log(res);
+        if(res.ok){
+            console.log("signed up successfully!")
+            location.replace("/profile")
+        } else {
+            alert("signup failed!")
+            location.reload();
+        }
+    })
+})
---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const User = require('./models/User');
+const Project = require('./models/Project');
+
+require("dotenv").config();
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge:1000*60*60*2
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public/')));
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+
+app.use(routes);
+
+app.get('/', (req,res) => {
+  res.render('index');
+});
+
+
+
+app.get('/profile', async(req, res)=>{
+   try{ 
+  const postData = await Project.findAll({
+      include: [{
+        model: User,
+        attributes: ['name'],
+      }]
+    });
+
+    const post = postData.map((user) => user.get({plain: true}));
+    console.log(post)
+    res.render('profile', {
+      post,
+    })
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+
+// app.get('/profile', async(req, res)=>{
+//   try{ 
+//  const postData = await User.findAll({
+//      include: [{
+//        model: Project,
+//        attributes: ['name', 'description'],
+//      }]
+//    });
+
+//    const post = postData.map((user) => user.get({plain: true}));
+//    console.log(post)
+//    res.render('profile', {
+//      post,
+//    })
+//  } catch (err) {
+//    console.log(err);
+//    res.status(500).json(err);
+//  }
+// });
