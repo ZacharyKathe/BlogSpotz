810571eb00498d875d9742aabb8088a978c69e93
ZacharyKathe <55865084+ZacharyKathe@users.noreply.github.com>
2021-05-03 20:23:13 -0700

    updating

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -46,6 +46,7 @@ router.post('/login', async (req, res) => {
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
+      res.replace("/profile")
     });
 
   } catch (err) {
